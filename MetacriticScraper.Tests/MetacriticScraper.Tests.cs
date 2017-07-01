ef89f23126ba6d864536afaa2a6c94bd081513a0
marcalencc <marcalencc@gmail.com>
2017-07-02 05:02:35 +0800

    Unit Testing

---
+++ MetacriticScraper.Tests/MetacriticScraper.Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MetacriticScraper.Tests
+{
+    [TestFixture]
+    public class ScraperTest
+    {
+        [Test]
+        public void ()
+        {
+        }
+    }
+}
