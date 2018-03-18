bc3ed1d08b9462540b90e612c0a1ad63a77689f9
Marc Alen Cuevas <marcalencc@gmail.com>
2018-03-19 00:42:37 +0800

    - Updated nuget version

--- MetacriticScraper/Properties/AssemblyInfo.cs
+++ MetacriticScraper/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MetacriticScraper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# library for scraping reviews and info including cast and credits of movies, albums and tv shows from Metacritic website")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("marcalencc")]
 [assembly: AssemblyProduct("MetacriticScraper")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.2.0")]
 [assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
